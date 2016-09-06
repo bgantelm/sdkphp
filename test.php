8a6715bfdc0f7ebcdc8976604d41ba5e747e9727
Bruno <bruno.gantelmi@recast.ai>
2016-09-06 11:10:50 +0200

    ajout

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once __DIR__ . './../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use recast\SayHello;
-
-echo SayHello::world();
+$lol = \Recast\SayHello::world();
+echo $lol;
