585fefecf7644bbe07142424b2c569b31d0b34d9
Bruno <bruno.gantelmi@recast.ai>
2016-09-06 10:15:39 +0200

    ajout

---
+++ src/SayHello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace recast;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . './../vendor/autoload.php';
+
+use recast\SayHello;
+
+echo SayHello::world();
